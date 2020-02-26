3d2e4427ef07014adb8801191d2972c9726e3b90
Katja Seikkinen <katjasei@metropolia.fi>
2020-02-26 21:48:40 +0200

    Added after_stop_activity layout

--- app/src/main/java/com/example/plogging/ui/home/AfterStopActivityFragment.kt
+++ app/src/main/java/com/example/plogging/ui/home/AfterStopActivityFragment.kt
@@ -1,6 +1,5 @@
 package com.example.plogging.ui.home
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +8,7 @@ import androidx.fragment.app.Fragment
 import com.example.plogging.R
 
 
-class PloggingActivityFragment: Fragment() {
+class AfterStopActivityFragment: Fragment() {
 
     /*
     private var activityCallBack: PloggingActivityListener? = null
@@ -25,7 +24,7 @@ class PloggingActivityFragment: Fragment() {
 */
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_plogging_activity, container, false)
+        return inflater.inflate(R.layout.fragment_after_stop_activity, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
