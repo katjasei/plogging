554f8e82aee71df1aba4080638809b9dc34a1e4a
Katja Seikkinen <katjasei@metropolia.fi>
2020-03-11 15:19:27 +0200

    Move functions from PloggingActivityFragment to HomeFragment

--- app/src/main/java/com/example/plogging/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/plogging/ui/home/HomeFragment.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.PorterDuff
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorManager
 import android.location.Location
 import android.os.Bundle
 import android.os.Handler
@@ -26,6 +29,11 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import androidx.lifecycle.Observer
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.maps.model.PolylineOptions
+import kotlinx.android.synthetic.main.fragment_home.floating_action_button
+import kotlinx.android.synthetic.main.fragment_plogging_activity.*
+import java.lang.Error
 
 
 class HomeFragment: Fragment(), OnMapReadyCallback  {
@@ -35,6 +43,23 @@ class HomeFragment: Fragment(), OnMapReadyCallback  {
     private var running= false
     private var seconds = 0
     private val loginViewModel = LoginViewModel()
+    private val step = 0.0007 //(km)
+    private var routeLength: Double = 0.0
+    private lateinit var startPoint: LatLng
+    var routePoints = mutableListOf<LatLng>()
+    private lateinit var marker: MarkerOptions
+    private lateinit var startMarker: MarkerOptions
+    private var stepsBeforeStart: Float = 1f
+    private var firstStep = true
+    private lateinit var locationMap: GoogleMap
+    private lateinit var lastLocation: Location
+    private lateinit var lastLocationLatLng: LatLng
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var locationRequest: LocationRequest
+    private var locationUpdateState = false
+    private lateinit var sensorManager: SensorManager
+    private var stepCounterSensor: Sensor? = null
+    private lateinit var currentLocation: LatLng
 
     interface HomeFragmentListener {
         fun onButtonStartActivityClick()
@@ -71,6 +96,50 @@ class HomeFragment: Fragment(), OnMapReadyCallback  {
             observeAuthenticationState()
         }
 
+        sensorManager = activity?.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        checkForStepCounterSensor()
+
+        //setup location callback (when location changes, do this)
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+                lastLocationLatLng = LatLng(lastLocation.latitude, lastLocation.longitude)
+                Log.i("location", "Location update: $lastLocationLatLng")
+
+                //markers cannot be removed, clear and draw everything again
+                locationMap.clear()
+
+                marker = MarkerOptions()
+                    .position(lastLocationLatLng)
+                    .title("Your current location")
+                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.dot))
+
+                startMarker = MarkerOptions()
+                    .position(startPoint)
+                    .title("Start point")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+
+                //add startMarker and current location marker
+                locationMap.addMarker(marker)
+                locationMap.addMarker(startMarker)
+
+                //move camera according to location update
+                locationMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
+
+                //add point to list
+                routePoints.add(lastLocationLatLng)
+
+                //add polyline between locations
+                locationMap.addPolyline(
+                    PolylineOptions()
+                        .addAll(routePoints)
+                        .width(20f).color(Color.parseColor("#801B60FE")).geodesic(true)
+                )
+            }
+        }
+        createLocationRequest()
+
         return view
     }
 
@@ -105,11 +174,66 @@ class HomeFragment: Fragment(), OnMapReadyCallback  {
         floating_action_button.setImageDrawable(willBeWhite)
     }
 
+   /*
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+        Log.i("sensor", "Accuracy changed")
+    }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor == stepCounterSensor) {
+
+            if (!firstStep) { //steps after the first
+                Log.i("sensor", "Sensor data: ${event.values[0]}")
+                stepTextView.text = (event.values[0] - stepsBeforeStart).toString()
+                routeLength = (event.values[0] - stepsBeforeStart)*step
+                updateRouteLength()
+
+            } else {  //first event, check the sensor value and set it to stepsBeforeStart to calculate steps during this plogging
+                stepTextView.text = "0"
+                stepsBeforeStart = event.values[0]
+                firstStep = false
+                updateRouteLength()
+            }
+        }
+    }
+
+    */
+
+    private fun updateRouteLength(){
+        val rounded = "%.1f".format(routeLength)
+        value_distance_activity.text = rounded
+    }
+
+
+    override fun onResume() {
+        super.onResume()
+        //register sensor listener
+        stepCounterSensor?.also {
+           // sensorManager.registerListener(this, it,
+         //       SensorManager.SENSOR_DELAY_NORMAL)
+        }
+        //start location updates if not already on
+        if (!locationUpdateState) {
+            startLocationUpdates()
+        }
+    }
+
     override fun onStart() {
         super.onStart()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(context!!.applicationContext)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        //start location updates
+        locationUpdateState = true
+        startLocationUpdates()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        //unregister sensor listener
+       // sensorManager.unregisterListener(this)
+        //remove location updates
+        fusedLocationProviderClient.removeLocationUpdates(locationCallback)
     }
 
     override fun onMapReady(map: GoogleMap) {
@@ -124,6 +248,56 @@ class HomeFragment: Fragment(), OnMapReadyCallback  {
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLocation, 15f))
         }
     }
+
+    private fun startLocationUpdates() {
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, null)
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+        locationRequest.interval = 10000
+        locationRequest.fastestInterval = 5000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+
+        try {
+            val client = LocationServices.getSettingsClient(this.requireActivity())
+            val task = client.checkLocationSettings(builder.build())
+
+            //On success
+            task.addOnSuccessListener {
+                locationUpdateState = true
+                startLocationUpdates()
+            }
+
+            //On failure
+            task.addOnFailureListener { e ->
+                if (e is ResolvableApiException) {
+                    Log.i("route", "Error in location settings")
+                } else {
+                    Log.e("route", "CheckLocationSettings task failed: "+e.message)
+                }
+            }
+        } catch (e: Error){
+            Log.e("route", "Error getting location updates: ${e.message}")
+        }
+    }
+
+    private fun checkForStepCounterSensor() {
+        if (sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER) != null) {
+            stepCounterSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+            Log.i("TAG", "Sensor found")
+        } else {
+            Log.i("TAG", "No sensor available")
+            //TODO disable sensor activity
+        }
+    }
+
+    fun resetStepCounter() {
+        firstStep = true
+    }
+
 
     //TODO: move this fun to separate file
     private fun runTimer(textView: TextView){
